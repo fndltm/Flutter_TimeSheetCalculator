5af553890c35e98d6b07edcd733df57110208a63
Pedro Frois Bittencourt <pedro.bittencourt@axxiom.com.br>
2020-04-13 17:36:51 -0300

    Animated SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,254 +1,42 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-import 'Widgets/SocialIcons.dart';
+import 'package:timesheet_calculator/Screens/Login.dart';
 
 void main() => runApp(MaterialApp(
-      home: MyApp(),
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     ));
 
-class MyApp extends StatefulWidget {
+class SplashScreen extends StatefulWidget {
   @override
-  _MyAppState createState() => new _MyAppState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _MyAppState extends State<MyApp> {
-  bool _isSelected = false;
-
-  void _toggleCheckbox() {
-    setState(() {
-      _isSelected = !_isSelected;
-    });
-  }
-
-  Widget horizontalLine() => Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0),
-        child: Container(
-          width: ScreenUtil().setWidth(120),
-          height: 1.0,
-          color: Colors.black26.withOpacity(.2),
-        ),
-      );
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
-    return new Scaffold(
-      backgroundColor: Colors.white,
-      resizeToAvoidBottomPadding: true,
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 20.0),
-                child: Image.asset("assets/image_01.png"),
-              ),
-              Expanded(
-                child: Container(),
-              ),
-              Image.asset("assets/image_02.png")
-            ],
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 150,
+          height: 150,
+          child: FlareActor(
+            "assets/clock_anim.flr",
+            animation: "Spin",
           ),
-          SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Image.asset(
-                        "assets/logo.png",
-                        width: ScreenUtil().setWidth(110),
-                        height: ScreenUtil().setHeight(110),
-                      ),
-                      Text("LOGO",
-                          style: TextStyle(
-                              fontFamily: "Poppins-Bold",
-                              fontSize: ScreenUtil().setSp(46),
-                              letterSpacing: .6,
-                              fontWeight: FontWeight.bold))
-                    ],
-                  ),
-                  SizedBox(
-                    height: ScreenUtil().setHeight(150),
-                  ),
-                  Container(
-                    width: ScreenUtil().setWidth(650),
-                    height: ScreenUtil().setHeight(580),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8.0),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black12,
-                              offset: Offset(0.0, 15.0),
-                              blurRadius: 15.0),
-                          BoxShadow(
-                              color: Colors.black12,
-                              offset: Offset(0.0, -10.0),
-                              blurRadius: 10.0),
-                        ]),
-                    child: Padding(
-                      padding:
-                          EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("Login",
-                              style: TextStyle(
-                                  fontSize: ScreenUtil().setSp(45),
-                                  fontFamily: "Poppins-Bold",
-                                  letterSpacing: .6)),
-                          SizedBox(
-                            height: ScreenUtil().setHeight(30),
-                          ),
-                          Text("Usuário",
-                              style: TextStyle(
-                                  fontFamily: "Poppins-Medium",
-                                  fontSize: ScreenUtil().setSp(26))),
-                          TextField(
-                            decoration: InputDecoration(
-                                hintText: "Usuário",
-                                hintStyle: TextStyle(
-                                    color: Colors.grey, fontSize: 12.0)),
-                          ),
-                          SizedBox(
-                            height: ScreenUtil().setHeight(30),
-                          ),
-                          Text("Senha",
-                              style: TextStyle(
-                                  fontFamily: "Poppins-Medium",
-                                  fontSize: ScreenUtil().setSp(26))),
-                          TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                hintText: "Senha",
-                                hintStyle: TextStyle(
-                                    color: Colors.grey, fontSize: 12.0)),
-                          ),
-                          SizedBox(
-                            height: ScreenUtil().setHeight(35),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              Material(
-                                color: Colors.transparent,
-                                child: InkWell(
-                                  onTap: () {}, // needed
-                                  child: Text(
-                                    "Esqueceu sua senha?",
-                                    style: TextStyle(
-                                        color: Colors.blue,
-                                        fontFamily: "Poppins-Medium",
-                                        fontSize: ScreenUtil().setSp(26)),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: ScreenUtil().setHeight(40)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      InkWell(
-                        child: Container(
-                          width: ScreenUtil().setWidth(650),
-                          height: ScreenUtil().setHeight(100),
-                          decoration: BoxDecoration(
-                              gradient: LinearGradient(colors: [
-                                Color(0xFF17ead9),
-                                Color(0xFF6078ea)
-                              ]),
-                              borderRadius: BorderRadius.circular(6.0),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Color(0xFF6078ea).withOpacity(.3),
-                                    offset: Offset(0.0, 8.0),
-                                    blurRadius: 8.0)
-                              ]),
-                          child: Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                              onTap: () {},
-                              child: Center(
-                                child: Text("login".toUpperCase(),
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontFamily: "Poppins-Bold",
-                                        fontSize: 18,
-                                        letterSpacing: 1.0)),
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: ScreenUtil().setHeight(30),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      horizontalLine(),
-                      Text("Entrar com",
-                          style: TextStyle(
-                              fontSize: 16.0, fontFamily: "Poppins-Medium")),
-                      horizontalLine()
-                    ],
-                  ),
-                  SizedBox(
-                    height: ScreenUtil().setHeight(20),
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        SocialIcons(
-                          svgIcon: SvgPicture.asset("assets/google-icon.svg",
-                              width: 36, height: 36),
-                          onPressed: () {},
-                        ),
-                        SocialIcons(
-                          svgIcon: SvgPicture.asset("assets/facebook-icon.svg",
-                              width: 36, height: 36),
-                          onPressed: () {},
-                        )
-                      ]),
-                  SizedBox(
-                    height: ScreenUtil().setHeight(30),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "Ainda não tem uma conta? ",
-                        style: TextStyle(fontFamily: "Poppins-Medium"),
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: Text("Cadastre-se",
-                            style: TextStyle(
-                                color: Colors.blueAccent,
-                                fontFamily: "Poppins-Bold")),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3)).then((_) {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => Login()));
+    });
+  }
+
 }
